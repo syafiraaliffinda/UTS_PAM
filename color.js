726e19fae6480402a4e87ac959264ef7536cca91
syafiraaliffinda <95162649+syafiraaliffinda@users.noreply.github.com>
2023-03-23 23:55:40 +0700

    Add files via upload

---
+++ color.js
@@ -0,0 +1,11 @@
+const Colors = {
+    // main: linear-gradient(182.83deg, #10BD01 2.36%, #75E16C 98.74%),
+    green: "#10BD01",
+    darkGreen: "#1E761C",
+    black: "#000",
+    gray: "#7B7B7B",
+    lightGray: "#D9D9D9",
+    transparanGray: 'rgba(120, 120, 120, 0.4)',
+    white: "#FFFF",
+
+};
